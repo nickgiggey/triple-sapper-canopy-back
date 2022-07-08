cd7aebc31232e02a817a144eeb8df9ba822a9676
xelphite <stoppanij@gmail.com>
2022-07-08 15:50:49 -0700

    got room routes working

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -13,14 +13,12 @@ const Room = require('../models/Room');
 //(.get , .post , .put , .patch , .delete)
 router.get('/', (req, res, next) => {
 	Room.find({})
-		.populate('everything')
-		.then((examples) => res.json(examples))
+		.then((rooms) => res.json(rooms))
 		.catch(next);
 });
 
 router.get('/:id', (req, res, next) => {
 	Room.findById(req.params.id)
-		.populate('everything')
 		.then((example) => res.json(example))
 		.catch(next);
 });
